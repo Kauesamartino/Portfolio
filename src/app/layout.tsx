8451868c055dda4e09450a4e360a7413cd633607
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-20 04:38:16 -0300

    fix: correct import path for InteractiveBackground component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Layout/Header";
-import InteractiveBackground from "./components/interactiveBackground";
+import InteractiveBackground from "./components/InteractiveBackground";
 
 export const metadata: Metadata = {
   title: "Create Next App",
