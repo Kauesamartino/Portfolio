85841b59ce7034fec2d96ed547722b42fd2ce5d9
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-22 01:42:36 -0300

    feat: update Projects component with new image and enhanced styles

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -1,22 +1,18 @@
-import Image from 'next/image';
 import style from './projects.module.scss';
 
 export const Projects = () => {
 
     return (
         <section className={style.section}>
-            <h1 className={style.title}>My Projects</h1>
-            <div className={style.containerImage}>
-                <Image 
-                    src="/images/coming-soon.png"
-                    alt="Coming Soon"
-                    width={300}
-                    height={300}
-                />
-            </div>
-            <div className={style.container}>
-                <h2 className={style.title}>Projects</h2>
-                <p className={style.description}>Details about projects will go here.</p>
+            <div className={style.flex}>
+                <h1 className={style.title}>My Projects</h1>
+                <div className={style.containerImage} style={{ backgroundImage: "url(/safe-flood.vercel.app_.png)" }}>
+                    <div className={style.overlay}>SafeFlood</div>
+                </div>
+                <div className={style.container}>
+                    <h2 className={style.title}>Projects</h2>
+                    <p className={style.description}>Details about projects will go here.</p>
+                </div>
             </div>
         </section>
     );
