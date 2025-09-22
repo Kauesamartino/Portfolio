01ef192e59af01ddf5b296972ed3a88d00182178
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-22 02:40:10 -0300

    feat: refactor Projects component layout and styles for improved responsiveness

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -1,17 +1,20 @@
+import Link from 'next/link';
 import style from './projects.module.scss';
 
 export const Projects = () => {
 
     return (
         <section className={style.section}>
+            <h1 className={style.title}>My Projects</h1>
             <div className={style.flex}>
-                <h1 className={style.title}>My Projects</h1>
-                <div className={style.containerImage} style={{ backgroundImage: "url(/safe-flood.vercel.app_.png)" }}>
-                    <div className={style.overlay}>SafeFlood</div>
-                </div>
+                <Link href="https://safe-flood.vercel.app/" target="_blank" rel="noopener noreferrer" className={style.containerImage} style={{ backgroundImage: "url(/safe-flood.vercel.app_.png)" }}>
+                    <div className={style.overlay}>
+                        SafeFlood
+                    </div>
+                </Link>
                 <div className={style.container}>
-                    <h2 className={style.title}>Projects</h2>
-                    <p className={style.description}>Details about projects will go here.</p>
+                    <h2 className={style.title}>SafeFlood</h2>
+                    <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit reiciendis neque molestias veritatis, aperiam minima ab in culpa corporis sapiente tempore incidunt ipsum adipisci minus provident id distinctio ex. Quae.</p>
                 </div>
             </div>
         </section>
