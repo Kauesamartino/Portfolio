7292f4f9e645e86e31b37974241a48d3468430fa
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-23 15:30:57 -0300

    feat: simplify Button component props by removing unnecessary attributes

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -4,23 +4,14 @@ import styles from './button.module.scss';
 type ButtonProps = {
 	children: React.ReactNode;
 	variant?: 'primary' | 'secondary';
-	as?: 'button' | 'a';
-	href?: string;
-	target?: string;
-	rel?: string;
 	onClick?: React.MouseEventHandler;
 	type?: 'button' | 'submit' | 'reset';
 	className?: string;
-	[key: string]: any;
 };
 
 export const Button: React.FC<ButtonProps> = ({
 	children,
 	variant = 'primary',
-	as = 'button',
-	href,
-	target,
-	rel,
 	onClick,
 	type = 'button',
 	className = '',
